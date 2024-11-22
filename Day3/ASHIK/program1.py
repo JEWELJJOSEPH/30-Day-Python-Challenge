3abad8c899b545ee6c8d72bd3e69665d1f410612
Ashik s <ashiks2028@cs.sjcetpalai.ac.in>
2024-11-22 22:56:54 +0530

    Update even and odd.py

--- Day3/ASHIK/program1.py
+++ Day3/ASHIK/program1.py
@@ -1,1 +1,10 @@
-
+a=[2,5,22,67,40,11,43,66,34,7]
+even=[]
+odd=[]
+for i in a:
+    if i%2==0:
+        even.append(i)
+    elif i%2!=0:
+        odd.append(i)
+print('EVEN NUMBERS ARE: ',even)
+print('ODD NUMMBERS ARE: ',odd)
